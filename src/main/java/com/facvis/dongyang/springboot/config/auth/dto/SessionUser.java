4abcd721ea1cbac2105bdde302878270f49039ff
yunaje028 <tjdbswo7@gmail.com>
2020-11-22 00:47:04 +0900

    구글 로그인 추가

---
+++ src/main/java/com/facvis/dongyang/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.facvis.dongyang.springboot.config.auth.dto;
+
+import com.facvis.dongyang.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user){
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}
